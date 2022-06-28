eb4790deab6bf591b54fd6512f15d66cdc464bb9
Anton Lisin <kandzaki@gmail.com>
2022-06-28 18:45:58 +0300

    lesson9

--- src/shared/Sidebar/sidebarConfig.ts
+++ src/shared/Sidebar/sidebarConfig.ts
@@ -9,4 +9,29 @@ export const sidebarConfig = [
     name: 'Тестовая страница',
     path: '/test_page',
   },
+  {
+    id: 3,
+    name: 'Список Todo',
+    path: '/todos_page',
+  },
+  {
+    id: 4,
+    name: 'Use Ref',
+    path: '/use_ref',
+  },
+  {
+    id: 5,
+    name: 'Use Callback',
+    path: '/use_callback',
+  },
+  {
+    id: 6,
+    name: 'Use Memo',
+    path: '/use_memo',
+  },
+  {
+    id: 7,
+    name: 'Use Context',
+    path: '/use_context',
+  },
 ];
